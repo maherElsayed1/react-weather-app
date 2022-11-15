8bdc3ddaf0b4218d16b7d288ef319a3a28abd80d
Maher Elsayed <maher.elsayed.dev@gmail.com>
2022-11-15 14:12:35 +0400

    update card types

--- src/components/Card/Card.types.ts
+++ src/components/Card/Card.types.ts
@@ -1,34 +1,35 @@
 export interface CardTypes {
     dt: number,
-    main: {
-        temp: number,
-        feels_like: number,
-        temp_min: number,
-        temp_max: number,
-        pressure: number,
-        sea_level: number,
-        grnd_level: number,
-        humidity: number,
-        temp_kf: number
+    sunrise: number,
+    sunset: number,
+    temp: {
+        day: number,
+        min: number,
+        max: number,
+        night: number,
+        eve: number,
+        morn: number
     },
-    weather: {
-        id: number,
-        main: string,
-        description: string,
-        icon: string
-    }[],
-    clouds: {
-        all: number
+    feels_like: {
+        day: number,
+        night: number,
+        eve: number,
+        morn: number
     },
-    wind: {
-        speed: number,
-        deg: number,
-        gust: number
-    },
-    visibility: number,
+    pressure: number,
+    humidity: number,
+    weather: [
+        {
+            id: number,
+            main: string,
+            description: string,
+            icon: string
+        }
+    ],
+    speed: number,
+    deg: number,
+    gust: number,
+    clouds: number,
     pop: number,
-    sys: {
-        pod: string
-    },
-    dt_txt: string
+    rain: number
 }
