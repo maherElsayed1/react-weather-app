e46635c1b8f480127c672564d68574d85f8f97e3
Maher Elsayed <maher.elsayed.dev@gmail.com>
2022-11-15 14:13:25 +0400

    Add card data and update its styles

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,3 +1,4 @@
+import moment from "moment"
 import React, { Component } from 'react'
 import "./Card.css"
 import { CardTypes } from "./Card.types"
@@ -10,12 +11,26 @@ type State = {}
 
 export default class Card extends Component<Props, State> {
     state = {}
-
     render() {
+        const { cardData } = this.props;
+        const date = moment.unix(cardData.dt);
+        const isToday = date.format('YYYY-MM-DD') === moment().format('YYYY-MM-DD');
+
         return (
             <div className="weather-card">
-                {this.props.cardData?.main.temp}
-                <div>{this.props.cardData?.weather[0]?.description}</div>
+                <div className="weather-card__title-container">
+                    <h4 className="weather-card__title">{isToday ? 'Today' : date.format('ddd')}</h4>
+                </div>
+                <div className="weather-card__results">
+                    <div className="weather-card__icon">
+                        <img src={`http://openweathermap.org/img/wn/${cardData?.weather[0]?.icon}@2x.png`} alt="" />
+                    </div>
+                    <div className="weather-card__temp">
+                        <span>{cardData?.temp.max}°/{cardData?.temp.min}°</span>
+                        {isToday && <span className="weather-card__temp-description">{cardData?.weather[0]?.description}</span>}
+                    </div>
+                </div>
+
             </div>
         )
     }
